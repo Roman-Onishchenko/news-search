5bda744f486c20af9402fe4cdbba2ffe97b39308
Roman-Onishencko <roman951t@gmail.com>
2017-07-31 18:47:33 +0300

    Add files via upload

---
+++ src/NewsMenu.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import * as actions from './actions';
+import { Link } from 'react-router-dom';
+import './styles/NewsList.css';
+
+class NewsMenu extends Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+    <div className="news-list">
+      <ul>
+        <li><Link to="/bbc-sport">Спортивные новости</Link></li>
+        <li><Link to="/the-economist">Финансовые новости</Link></li>
+        <li><Link to="/techradar">Новости науки</Link></li>
+        <li><Link to="/usa-today">Зарубежные новости</Link></li>
+        <li><Link to="/google-news">Новости культуры</Link></li>
+        <li><Link to="/fortune">Разное</Link></li>
+      </ul>
+    </div>
+  )}
+}
+
+export default connect(
+  state => ({
+
+  }),
+  dispatch => bindActionCreators(actions, dispatch)
+)(NewsMenu);
--- src/fetchNewsSaga.js
+++ src/fetchNewsSaga.js
@@ -2,14 +2,14 @@ import { call, put, takeEvery, takeLatest } from 'redux-saga/effects';
 import { push } from 'connected-react-router';
 import * as types from "./constants";
 import Api from './api/api.js';
-import { fetchSuccess, fetchFail, fetchMoviesData } from './actions';
+import { fetchSuccess, fetchFail, preloadToFalse } from './actions';
 import { List, Map } from 'immutable';
 
 function* fetchNews(action) {
   const api = new Api();
 
   try {
-    let news = yield call(api.fetchNews, action.title);
+    let news = yield call(api.fetchNews, action.source);
      yield put(fetchSuccess(news));
    } catch (e) {
      yield put(fetchFail(e.message));
