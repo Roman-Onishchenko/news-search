7a6132e15209e102b65d86c8b5dbd403297b218d
Roman-Onishencko <roman951t@gmail.com>
2017-07-31 18:52:09 +0300

    Add files via upload

--- src/api/api.js
+++ src/api/api.js
@@ -6,9 +6,9 @@ export default class Api {
     this.fetchNews = this.fetchNews.bind(this);
   }
 
-  fetchNews() {
+  fetchNews(source) {
     return new Promise((resolve, reject) => {
-      const url = 'https://newsapi.org/v1/articles?source=bbc-sport&sortBy=top&apiKey=23c4065645124c6ab598cd7d04086961';
+      const url = `https://newsapi.org/v1/articles?source=${source}&sortBy=top&apiKey=23c4065645124c6ab598cd7d04086961`;
 
       fetch(url).then((response) => {
          return response.json();
